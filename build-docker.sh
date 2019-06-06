47d74be8d7b7dfe5fca8f9af7f289f4df76fcd5d
Matt <mdang808@gmail.com>
2019-06-06 16:35:05 -0400

    LORE-96 Replace build script with fabric8 Docker plugin

--- build-docker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# builds the project and creates the JAR file
-mvn clean install
-# uses the Dockerfile and names the resulting image "transformation-service-registry"
-docker build -t transformation-service-registry -f ./Dockerfile .
